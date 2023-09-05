6b935c2a5640e70d36596f5350af1be88a6b7927
mohamed_sakr <sakr83117@gmail.com>
2023-09-05 16:01:02 +0300

    created register form with validators

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    first_name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+    ]),
+    last_name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+    ]),
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.min(16),
+      Validators.max(70),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
+  formSubmit(registerForm: FormGroup) {
+    console.log(registerForm.value);
+  }
 }
